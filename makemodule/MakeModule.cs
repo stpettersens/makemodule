c0dd83bedf3922161dfb5f759cff189c19aa9a05
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-05-13 18:54:54 +0100

    Remove unused usings.

--- makemodule/MakeModule.cs
+++ makemodule/MakeModule.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Collections;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 using System.IO;
 using System.Xml;
